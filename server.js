520f81b0567d219d69ef2c6e699d36c44d40a4bd
slowy-slowy <94172671+slowy-slowy@users.noreply.github.com>
2025-08-09 13:53:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -56,4 +56,6 @@ function setEffectiveRankVal(cards) {
   return RANK_VAL[others[0]];
 }
 
-server.listen(3000, () => console.log('Server draait op poort 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server draait op poort ${PORT}`));
+
